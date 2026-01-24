3ef28a4ac74c1acd61f2c72f7c7047123f5bd669
Robert Muchiri <waburimuchiri@gmail.com>
2026-01-24 23:50:12 +0300

    Update cmd/api/main.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -29,6 +29,7 @@ func main() {
 
 	required := map[string]string{
 		"PORT":              os.Getenv("PORT"),
+		"DATABASE_PORT":     os.Getenv("DATABASE_PORT"),
 		"DATABASE_HOST":     os.Getenv("DATABASE_HOST"),
 		"DATABASE_USER":     os.Getenv("DATABASE_USER"),
 		"DATABASE_PASSWORD": os.Getenv("DATABASE_PASSWORD"),
