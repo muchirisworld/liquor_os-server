48cd8ec76c13a6b840a4bbe781bf71a8e0f3835b
Robert Muchiri <waburimuchiri@gmail.com>
2026-01-26 00:39:57 +0300

    Update internal/domain/user.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Name          string    `db:"name" json:"name"`
 	Email         string    `db:"email" json:"email"`
 	EmailVerified bool      `db:"email_verified" json:"email_verified"`
-	Image         string    `db:"image" json:"image"`
+	Image         *string   `db:"image" json:"image"`
 	CreatedAt     time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
 }
