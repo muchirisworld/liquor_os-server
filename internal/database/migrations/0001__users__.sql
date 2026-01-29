97d5a6919a5d2523950457f1c046a6e55c0e6fc5
muchirisworld <waburimuchiri@gmail.com>
2026-01-29 16:33:14 +0300

    refactor: update users db migration

--- internal/database/migrations/0001__users__.sql
+++ internal/database/migrations/0001__users__.sql
@@ -1,27 +1,42 @@
--- +goose up
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS citext;
 
-CREATE TABLE "user" (
-	"id" varchar PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"email" email_citext NOT NULL UNIQUE,
-	"email_verified" boolean DEFAULT false NOT NULL,
-	"image" text,
-	"created_at" timestamptz DEFAULT now() NOT NULL,
-	"updated_at" timestamptz DEFAULT now() NOT NULL
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'email_citext') THEN
+        CREATE DOMAIN email_citext AS citext
+            CHECK (VALUE ~* '^[A-Z0-9._%+\-]+@[A-Z0-9.\-]+\.[A-Z]{2,63}$');
+    END IF;
+END$$;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION trg_set_updated_at()
+RETURNS trigger LANGUAGE plpgsql AS $$
+BEGIN
+    NEW.updated_at := now();
+    RETURN NEW;
+END$$;
+-- +goose StatementEnd
+
+CREATE TABLE users (
+    "id" varchar PRIMARY KEY NOT NULL,
+    "name" text NOT NULL,
+    "email" email_citext NOT NULL UNIQUE,
+    "email_verified" boolean DEFAULT false NOT NULL,
+    "image" text,
+    "created_at" timestamptz DEFAULT now() NOT NULL,
+    "updated_at" timestamptz DEFAULT now() NOT NULL
 );
 
--- +goose statementbegin
+-- +goose StatementBegin
 CREATE TRIGGER trg_users_updated_at
-BEFORE UPDATE ON "user"
+BEFORE UPDATE ON "users"
 FOR EACH ROW EXECUTE FUNCTION trg_set_updated_at();
+-- +goose StatementEnd
 
-CREATE TRIGGER trg_users_normalize_email
-BEFORE INSERT OR UPDATE ON "user"
-FOR EACH ROW EXECUTE FUNCTION trg_normalize_email();
--- +goose statementend
-
--- +goose down
-DROP TABLE IF EXISTS "user";
+-- +goose Down
+DROP TABLE IF EXISTS "users";
 DROP DOMAIN IF EXISTS email_citext;
 DROP FUNCTION IF EXISTS trg_set_updated_at;
-DROP FUNCTION IF EXISTS trg_normalize_email;
