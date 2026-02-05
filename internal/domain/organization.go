8257cbb5038bb7f8ef3d24d7d0f23e5d76c015f3
Robert Muchiri <waburimuchiri@gmail.com>
2026-02-05 05:39:03 +0300

    Update internal/domain/organization.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- internal/domain/organization.go
+++ internal/domain/organization.go
@@ -12,7 +12,7 @@ type Organization struct {
 	CreatedBy             string          `json:"created_by" db:"created_by"`
 	LogoURL               string          `json:"logo_url" db:"logo_url"`
 	ImageURL              string          `json:"image_url" db:"image_url"`
-	Metadata              json.RawMessage `json:"-" db:"metadata"`
+	Metadata              json.RawMessage `json:"metadata" db:"metadata"`
 	MaxAllowedMemberships int32           `json:"max_allowed_memberships" db:"max_allowed_memberships"`
 	CreatedAt             time.Time       `json:"created_at" db:"created_at"`
 	UpdatedAt             time.Time       `json:"updated_at" db:"updated_at"`
@@ -25,6 +25,6 @@ type OrganizationRequest struct {
 	CreatedBy             string          `json:"created_by" db:"created_by"`
 	LogoURL               string          `json:"logo_url" db:"logo_url"`
 	ImageURL              string          `json:"image_url" db:"image_url"`
-	Metadata              json.RawMessage `json:"-" db:"metadata"`
+	Metadata              json.RawMessage `json:"metadata" db:"metadata"`
 	MaxAllowedMemberships int32           `json:"max_allowed_memberships" db:"max_allowed_memberships"`
 }
